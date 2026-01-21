1639544ecfeb889a62df410131f8ff72eb193a84
Srinivasareddy Munnam <munnamsrinivasareddy09@gmail.com>
2026-01-21 13:29:46 +0530

    server-restart

--- server/server.js
+++ server/server.js
@@ -1,7 +1,39 @@
-const app = require("./app");
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const connectDB = require("./config/db.js");
+const imageRoutes = require("./routes/imageRoutes.js");
 const dotenv = require("dotenv");
+const app = require("./app");
 
 dotenv.config();
+
+// Connect to database
+connectDB();
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Static folder
+app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// Routes
+app.use("/api/images", imageRoutes);
+app.get("/",(req , res) =>
+{
+res.json({message:"hi from server"});
+})
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+
+
 
 const PORT = process.env.PORT || 5000;
 
