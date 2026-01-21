025bb6282ca74452da9ce36013458de9250bd58b
Srinivasareddy Munnam <munnamsrinivasareddy09@gmail.com>
2026-01-21 14:34:25 +0530

    deployed

--- server/server.js
+++ server/server.js
@@ -1,39 +1,69 @@
+// const express = require("express");
+// const cors = require("cors");
+// const path = require("path");
+// const connectDB = require("./config/db.js");
+// const imageRoutes = require("./routes/imageRoutes.js");
+// const dotenv = require("dotenv");
+// const app = express();
+
+// dotenv.config();
+
+// // Connect to database
+// connectDB();
+// // Middleware
+// app.use(cors());
+// app.use(express.json());
+
+// // Static folder
+// app.use("/uploads", express.static(path.join(__dirname, "uploads")));
+
+// // Routes
+// app.use("/api/images", imageRoutes);
+// app.get("/",(req , res) =>
+// {
+// res.json({message:"hi from server"});
+// })
+// // Error handling middleware
+// app.use((err, req, res, next) => {
+//   console.error(err.stack);
+//   res.status(500).send("Something broke!");
+// });
+
+
+
+
+// const PORT = process.env.PORT || 5000;
+
+// app.listen(PORT, () => {
+//   console.log(`Server running on port ${PORT}`);
+// });
+
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
-const connectDB = require("./config/db.js");
-const imageRoutes = require("./routes/imageRoutes.js");
-const dotenv = require("dotenv");
+const connectDB = require("./config/db");
+const imageRoutes = require("./routes/imageRoutes");
+require("dotenv").config();
+
 const app = express();
 
-dotenv.config();
-
-// Connect to database
+// DB
 connectDB();
+
 // Middleware
 app.use(cors());
 app.use(express.json());
 
-// Static folder
-app.use("/uploads", express.static(path.join(__dirname, "uploads")));
-
-// Routes
+// API routes
 app.use("/api/images", imageRoutes);
-app.get("/",(req , res) =>
-{
-res.json({message:"hi from server"});
-})
-// Error handling middleware
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something broke!");
+
+// Serve frontend
+const __dirname1 = path.resolve();
+app.use(express.static(path.join(__dirname1, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname1, "dist", "index.html"));
 });
-
-
-
 
 const PORT = process.env.PORT || 5000;
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(PORT, () => console.log(`Server running on ${PORT}`));
